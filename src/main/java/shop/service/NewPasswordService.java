b671f5e10f66f56cc26485d7313b9705d1fb09b2
mirozspace <zstefchev@gmail.com>
2020-12-20 17:36:21 +0100

    Remove password generator.

--- src/main/java/shop/service/NewPasswordService.java
+++
@@ -1,9 +0,0 @@
-package shop.service;
-
-import shop.models.service.NewPasswordServiceModel;
-
-public interface NewPasswordService {
-
-    String getRandomPassword(NewPasswordServiceModel newPasswordServiceModel);
-
-}
--- src/main/java/shop/web/controllers/HomeController.java
+++ src/main/java/shop/web/controllers/HomeController.java
@@ -2,15 +2,9 @@ package shop.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import shop.models.views.CategoryViewModel;
 import shop.models.views.ProductViewModel;
